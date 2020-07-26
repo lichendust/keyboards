b3496418df49ea054bd80e8c6744326148e5c5d8
Harley Denham <45082543+qxoko@users.noreply.github.com>
2020-07-26 20:51:02 +0100

    remove procreate layer

--- keymaps/undertow.c
+++ keymaps/undertow.c
@@ -2,7 +2,6 @@
 
 enum layers {
 	_BL = 1, // blender
-	_PR = 2, // procreate
 	_NP = 6, // numpad layer
 	_FN = 5, // main fn layer
 	_CT = 9, // control layer
@@ -49,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		LT(_NP,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
 		MO(_FN),        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   TD(HOME), KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_ENT,
-		KC_LSFT,        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_AMPR, KC_UP,   KC_RSFT,
-		KC_LCTL,        KC_LALT, KC_LGUI,                           KC_SPC,                              KC_RGUI, MO(_CT), KC_LEFT, KC_DOWN, KC_RGHT
+		KC_LSFT,        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS, KC_UP,   KC_RSFT,
+		KC_LGUI,        KC_LALT, KC_LCTL,                           KC_SPC,                              KC_RCTL, MO(_CT), KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[_FN] = UNDERTOW(
 	//  1        2        3        4        5        6        7        8        9        10       11       12       13       14       15
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		_______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, KC_INS,
-		_______, _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_END,  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,          _______,
-		KC_CAPS, _______, KC_BRID, KC_BRIU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______,
+		_______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______, _______,          _______,
+		KC_CAPS, _______, KC_BRID, KC_BRIU, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______,                            _______,                            _______, _______, _______, _______, _______
 	),
 
@@ -75,27 +74,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// blender
 	[_BL] = UNDERTOW(
 	//  1        2        3        4        5        6        7        8           9        10       11       12       13       14       15
-		_______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, KC_F11,  KC_F12,           KC_DEL,
-		MO(_NP), XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		MO(_FN), S(KC_A), _______, XXXXXXX, XXXXXXX, _______, _______, S(A(KC_A)), A(KC_A), A(KC_A), XXXXXXX, XXXXXXX, XXXXXXX,          _______,
-		XXXXXXX, _______, G(KC_Z), _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, KC_F11,  KC_F12,           XXXXXXX,
+		MO(_NP), XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, KC_F13,     KC_F14,  KC_F15,  _______, XXXXXXX, XXXXXXX,
+		MO(_FN), S(KC_A), _______, A(KC_I), XXXXXXX, _______, _______, S(A(KC_A)), A(KC_A), A(KC_A), XXXXXXX, XXXXXXX, XXXXXXX,          _______,
+		_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______,
 		_______, _______, _______,                            _______,                               _______, _______, _______, _______, _______
-	),
-
-	// procreate
-	[_PR] = UNDERTOW(
-	//  1        2           3           4        5        6        7        8        9        10       11       12       13       14       15
-		G(KC_D), KC_V,       G(KC_SCLN), G(KC_0), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-		XXXXXXX, KC_LBRC,    KC_RBRC,    KC_E,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		XXXXXXX, KC_A,       KC_S,       KC_B,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-		KC_LSFT, SGUI(KC_Z), G(KC_Z),    KC_X,    KC_C,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		G(KC_U), _______,    _______,                               _______,                            XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
 	),
 
 	// control layer
 	[_CT] = UNDERTOW(
 	//  1        2        3        4        5        6        7        8        9        10       11       12       13        14      15
-		RESET,   EEP_RST, RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+		RESET,   EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(_PR), XXXXXXX,
