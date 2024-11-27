abd6c809be56396d9a8406df9e1c2e9dfb9c1282
Harley Denham <45082543+qxoko@users.noreply.github.com>
2024-11-27 20:33:32 +0000

    initial commit

---
+++ source.c
@@ -0,0 +1,27 @@
+#include QMK_KEYBOARD_H
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+	switch (index) {
+	case 0:
+		if (clockwise) {
+			tap_code(KC_VOLU);
+		} else {
+			tap_code(KC_VOLD);
+		}
+		break;
+	case 1:
+		if (clockwise) {
+			tap_code(KC_LEFT);
+		} else {
+			tap_code(KC_RIGHT);
+		}
+		break;
+	case 2:
+		if (clockwise) {
+			tap_code(KC_PGDN);
+		} else {
+			tap_code(KC_PGUP);
+		}
+		break;
+	}
+}
