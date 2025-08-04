3b46d783de94a5f61584de4c30011179982ae154
Harley Denham <45082543+qxoko@users.noreply.github.com>
2025-08-04 23:22:30 +0100

    encoder program

--- source.c
+++ source.c
@@ -1,49 +1,21 @@
 #include QMK_KEYBOARD_H
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-	/*switch (get_highest_layer(layer_state)) {
+	switch (get_highest_layer(layer_state)) {
 	case 0:
-		switch (index) {
-		// top right
-		case 0:
-			register_code(KC_LEFT_CTRL);
-			register_code(KC_LEFT_SHIFT);
-			clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
-			unregister_code(KC_LEFT_SHIFT);
-			unregister_code(KC_LEFT_CTRL);
-			break;
-
-		// top left
-		case 1:
-			register_code(KC_LEFT_SHIFT);
-			clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
-			unregister_code(KC_LEFT_SHIFT);
-			break;
-
-		// big boi
-		case 2:
-			clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
-			break;
-		}
+		clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
 		break;
+
 	case 1:
-		switch (index) {
-		// top right
-		case 0:
-			clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
-			break;
-
-		// top left
-		case 1:
-			clockwise ? tap_code(QK_MOUSE_WHEEL_RIGHT) : tap_code(QK_MOUSE_WHEEL_LEFT);
-			break;
-
-		// big boi
-		case 2:
-			clockwise ? tap_code(QK_MOUSE_WHEEL_DOWN) : tap_code(QK_MOUSE_WHEEL_UP);
-			break;
-		}
+		register_code(KC_LEFT_SHIFT);
+		clockwise ? tap_code(KC_RIGHT) : tap_code(KC_LEFT);
+		unregister_code(KC_LEFT_SHIFT);
 		break;
-	}*/
+
+	case 2:
+		clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
+		break;
+	}
+
 	return false;
 }
